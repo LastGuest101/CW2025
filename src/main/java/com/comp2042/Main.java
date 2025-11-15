150d26f62386f3aef994e3882708c6f40de56d88
Jacob Villegas <psyjv6@nottingham.ac.uk>
2025-11-15 23:38:44 +0800

    Rename variable - renamed c into controller to make it more meaningful

--- src/main/java/com/comp2042/Main.java
+++ src/main/java/com/comp2042/Main.java
@@ -20,13 +20,13 @@ public class Main extends Application {
         ResourceBundle resources = null;
         FXMLLoader fxmlLoader = new FXMLLoader(location, resources);
         Parent root = fxmlLoader.load();
-        GuiController c = fxmlLoader.getController();
+        GuiController controller = fxmlLoader.getController();
 
         primaryStage.setTitle("TetrisJFX");
         Scene scene = new Scene(root, 300, 510);
         primaryStage.setScene(scene);
         primaryStage.show();
-        new GameController(c);
+        new GameController(controller);
     }
     /* Initialises the GameController and the GUI Controller, and sets the dimensions and the title of the window
 
